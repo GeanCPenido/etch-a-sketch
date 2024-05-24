0f17cb852d8a1f728b45c94b90408c06f9d58136
Penido <geanc.penido@gmail.com>
2024-05-24 17:58:25 -0300

    add hover

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function divCreator() {
     for (i = 0; i < 256; i++) {        
         const div = document.createElement("div");
         div.classList.add("divGrid");
-        div.style.minWidth = "6%";
         div.innerHTML = i;
         container.appendChild(div);
     }
